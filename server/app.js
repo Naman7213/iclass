f05ef64c7ff1a8b9335c1d3c6ed8735aac53b5b3
Naman7213 <72452798+Naman7213@users.noreply.github.com>
2022-12-04 13:45:09 +0530

    connection with database successful

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+require("./DB/conn");
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Reply from /" });
